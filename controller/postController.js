1a5fbd881e9dade56062a220878bdbc277954d38
RaviGhelani <92327082+RaviGhelani@users.noreply.github.com>
2022-07-16 18:08:16 +0530

    first

--- controller/postController.js
+++ controller/postController.js
@@ -1,6 +1,10 @@
 
 const { Post } = require('../module/post');
 
+
+// let allPost = await Post.find().lean();
+///loop 
+// post.likeCount = like.length;
 async function addPost(post) {
     let newPost = new Post(post);
     await newPost.save();
@@ -8,25 +12,29 @@ async function addPost(post) {
 }
 
 async function getAllPost() {
-    let allPost = await Post.find();
+    let allPost = await Post.find().lean();
+    for (let i = 0; i < allPost.length; i++) {
+        allPost[i]["likeCount"] = allPost[i].like.length;
+    }
+
     return allPost;
 }
 
 async function likePost(userId, postId) {
-    // let allPost = await Post.find().lean();
-    ///loop 
-    // post.likeCount = like.length;
-    console.log(postId)
     let like = await Post.updateOne(
-        { _id: postId },
+        { _id: postId, like: { $nin: userId } },
         {
             $push: {
                 like: userId
-                }
+            }
         })
+    console.log(like);
 
-    return like;
+    let allLike = await Post.findById({ _id: postId })
+    likeCount = allLike.length;
+    console.log("likeCount", likeCount)
 }
+
 
 module.exports = {
     addPost: addPost,
