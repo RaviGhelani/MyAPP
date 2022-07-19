e08f23bb613de385fd55fb39695585c73310525a
RaviGhelani <ravighelani1411@gmail.com>
2022-07-19 11:28:31 +0530

    first

--- router/globleChat.js
+++ router/globleChat.js
@@ -8,13 +8,13 @@ const { Profile } = require('../module/profile')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/', urlencodedParser, queryAuth, async function (req, res) {
-    let profile = await Profile.findOne({ userId: req.user._id })
-    if(!profile){
-        res.render('addProfile');
-    }
-    else{
-    res.render('chat', profile);
-    }
+    // let profile = await Profile.findOne({ userId: req.user._id })
+    // if(!profile){
+    //     res.render('addProfile');
+    // }
+    
+    res.render('chat');
+    
 });
 
 
